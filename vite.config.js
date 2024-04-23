9704ee600beb957ba9c7f2638850124b2d719c00
yeonghoon123 <haa3163314@naver.com>
2024-04-23 22:36:17 +0900

    fixed/ build시 reload시 404 error 해결

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                details: resolve(__dirname, "details/index.html"),
-            },
-        },
-    },
+    base: "/",
 });
