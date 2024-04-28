a000dd75989ff9db0081e9442cb861a68f38ccb6
yeonghoon123 <haa3163314@naver.com>
2024-04-28 18:10:06 +0900

    firebase delete

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/index.css";
 import "./utils/firebase";
-import { getAnalytics, logEvent } from "firebase/analytics";
-
-const analytics = getAnalytics();
-logEvent(analytics, "notification_received");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
